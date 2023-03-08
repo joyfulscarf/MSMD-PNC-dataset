afd74ead6f39448bb777ac028b8b4273cc394871
joyfulscarf <90243503+joyfulscarf@users.noreply.github.com>
2023-03-08 19:40:00 +0800

    Update unet_parts.py

--- unet_parts.py
+++ unet_parts.py
@@ -85,16 +85,6 @@ class Up(nn.Module):
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
-        # input is CHW
-        #diffY = torch.tensor([x2.size()[2] - x1.size()[2]])
-        #diffX = torch.tensor([x2.size()[3] - x1.size()[3]])
-        
-#         diffY = x2.size()[2] - x1.size()[2]
-#         diffX = x2.size()[3] - x1.size()[3]
-#         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-#                         diffY // 2, diffY - diffY // 2])
-#         x = torch.cat([x2, x1], dim=1)
-#         return self.conv(x)
     
         diffY = torch.tensor([x2.size()[2] - x1.size()[2]])
         diffX = torch.tensor([x2.size()[3] - x1.size()[3]])
